b34e3c0d1d3ce2a95d61a9ed0401a029f6de72da
huqiang <huqiang1990@outlook.com>
2019-12-19 20:20:20 +0800

    fix

--- Assets/Core/HGUI/UserEvent.cs
+++ Assets/Core/HGUI/UserEvent.cs
@@ -1,4 +1,5 @@
-﻿using huqiang.UIEvent;
+﻿using huqiang;
+using huqiang.UIEvent;
 using System;
 using UnityEngine;
 
@@ -26,7 +27,7 @@ namespace Assets.Core.HGUI
                 }
             }
         }
-        static bool DispatchEvent(GUIElement[] pipeLine,int index,Vector3 pos,Vector3 scale,Quaternion quate, UserAction action)
+        static bool DispatchEvent(GUIElement[] pipeLine, int index,Vector3 pos,Vector3 scale,Quaternion quate, UserAction action)
         {
             Vector3 p = quate * pipeLine[index].localPosition;
             Vector3 o = Vector3.zero;
@@ -38,8 +39,85 @@ namespace Assets.Core.HGUI
             Quaternion q = quate * pipeLine[index].localRotation;
             s.x *= scale.x;
             s.y *= scale.y;
+            var script = pipeLine[index].script;
+            if (script != null)
+            {
+                var ue = script.userEvent;
+                if (ue == null)
+                {
+                    int c= pipeLine[index].childCount;
+                    if(c > 0)
+                    {
+                        int os = pipeLine[index].childOffset + c;
+                        for (int i = 0; i < c; i++)
+                        {
+                            os--;
+                            if (DispatchEvent(pipeLine, os, o, s, q, action))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                else if (ue.forbid)
+                {
 
+                }
+                else
+                {
+
+                }
+            }
             return false;
         }
+        int xTime;
+        int yTime;
+        float lastX;
+        float lastY;
+        public Vector2 boxSize;
+        Vector2 maxVelocity;
+        Vector2 sDistance;
+        Vector2 mVelocity;
+        public float ScrollDistanceX
+        {
+            get { return sDistance.x; }
+            set
+            {
+                if (value == 0)
+                    maxVelocity.x = 0;
+                else
+                    maxVelocity.x = MathH.DistanceToVelocity(DecayRateX, value);
+                mVelocity.x = maxVelocity.x;
+                sDistance.x = value;
+                xTime = 0;
+                lastX = 0;
+            }
+        }
+        public float ScrollDistanceY
+        {
+            get { return sDistance.y; }
+            set
+            {
+                if (value == 0)
+                    maxVelocity.y = 0;
+                else
+                    maxVelocity.y = MathH.DistanceToVelocity(DecayRateY, value);
+                mVelocity.y = maxVelocity.y;
+                sDistance.y = value;
+                yTime = 0;
+                lastY = 0;
+            }
+        }
+        public float DecayRateX = 0.998f;
+        public float DecayRateY = 0.998f;
+        public float speed = 1f;
+        public Vector2 RawPosition { get; protected set; }
+        Vector2 LastPosition;
+        public int HoverTime { get; protected set; }
+        public long pressTime { get; internal set; }
+        public long entryTime { get; protected set; }
+        public long stayTime { get; protected set; }
+        public bool Pressed { get; internal set; }
+        public bool forbid;
     }
 }

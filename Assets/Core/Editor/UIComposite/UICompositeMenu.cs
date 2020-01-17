0cbbf0c0664ae0d7282340a43b253afdb7784e7e
huqiang <huqiang1990@outlook.com>
2020-01-17 14:49:54 +0800

    fix

--- Assets/Core/Editor/UIComposite/UICompositeMenu.cs
+++ Assets/Core/Editor/UIComposite/UICompositeMenu.cs
@@ -35,8 +35,10 @@ public static class UICompositeMenu
         trans.localPosition = Vector3.zero;
         trans.localScale = Vector3.one;
         trans.localRotation = Quaternion.identity;
-        go.AddComponent<MeshFilter>();
-        go.AddComponent<MeshRenderer>();
+        var can = go.AddComponent<Canvas>();
+        can.renderMode = RenderMode.ScreenSpaceOverlay;
+        can.additionalShaderChannels = AdditionalCanvasShaderChannels.TexCoord1 | AdditionalCanvasShaderChannels.TexCoord2 | AdditionalCanvasShaderChannels.TexCoord3;
+        go.AddComponent<CanvasRenderer>();
     }
     [MenuItem("GameObject/HGUI/Empty", false, 1)]
     static public void AddEmpty(MenuCommand menuCommand)

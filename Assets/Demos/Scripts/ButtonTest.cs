d084b9603c81f229d6e19dc3dd030134a027ebc5
hu <Hxwy0213@outlook>
2020-01-17 18:10:26 +0800

    fix

---
+++ Assets/Demos/Scripts/ButtonTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : TestHelper
+{
+    public override void LoadTestPage()
+    {
+        //Application.targetFrameRate = 60;
+#if UNITY_IPHONE || UNITY_ANDROID
+        //Scale.DpiScale = true;
+#endif
+#if UNITY_EDITOR
+        UIPage.LoadPage<ButtonPage>();
+#else
+        ElementAsset.LoadAssetsAsync("base.unity3d",(o,e)=> { UIPage.LoadPage<AniTestPage>(); });
+#endif
+    }
+}

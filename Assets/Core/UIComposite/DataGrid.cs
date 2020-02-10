7167af06d0576809535f56a4c1b21b0e712d9331
huqiang <huqiang1990@outlook.com>
2020-02-10 10:39:28 +0800

    fix

--- Assets/Core/UIComposite/DataGrid.cs
+++ Assets/Core/UIComposite/DataGrid.cs
@@ -2,16 +2,47 @@
 using huqiang.Data;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using UnityEngine;
 
 namespace huqiang.UIComposite
 {
+    public class DataGridItem
+    {
+        public UIElement Item;
+        public HText Text;
+    }
+    public class DataGridItemContext
+    {
+        public string Text;
+    }
+    public class DataGridColumn
+    {
+        public string Head;
+        public List<DataGridItemContext> datas = new List<DataGridItemContext>();
+    }
     public class DataGrid:Composite
     {
+        /// <summary>
+        /// 当前滚动的位置
+        /// </summary>
+        public Vector2 Position;
+        FakeStruct ItemMod;
+        FakeStruct HeadMod;
+        FakeStruct Drag;
+        FakeStruct Line;
+        public List<DataGridColumn> BindingData = new List<DataGridColumn>();
         public override void Initial(FakeStruct mod, UIElement element)
         {
             base.Initial(mod, element);
+            HeadMod = HGUIManager.FindChild(mod, "Head");
+            ItemMod = HGUIManager.FindChild(mod, "Item");
+            Drag = HGUIManager.FindChild(mod, "Drag");
+            Line = HGUIManager.FindChild(mod, "Line");
+        }
+
+        void Order()
+        {
+
         }
     }
 }

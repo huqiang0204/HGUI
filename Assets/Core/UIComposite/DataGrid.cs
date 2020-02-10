153f8f323724c934003f3244c4b768e9f4b3c17b
huqiang <huqiang1990@outlook.com>
2020-02-10 16:06:54 +0800

    fix

--- Assets/Core/UIComposite/DataGrid.cs
+++ Assets/Core/UIComposite/DataGrid.cs
@@ -8,8 +8,10 @@ namespace huqiang.UIComposite
 {
     public class DataGridItem
     {
+        public GameObject target;
         public UIElement Item;
         public HText Text;
+        public DataGridItemContext Context;
     }
     public class DataGridItemContext
     {
@@ -17,8 +19,37 @@ namespace huqiang.UIComposite
     }
     public class DataGridColumn
     {
+        /// <summary>
+        /// 宽度
+        /// </summary>
+        public float width = 80;
         public string Head;
         public List<DataGridItemContext> datas = new List<DataGridItemContext>();
+    }
+    public class DGConstructor
+    {
+        public UIInitializer initializer;
+        public virtual DataGridItem Create() { return null; }
+        public virtual void Update(DataGridItem obj, DataGridItemContext dat) { }
+    }
+    public class DGMiddleware<T, U> : DGConstructor where T : DataGridItem, new() where U : DataGridItemContext, new()
+    {
+        public DGMiddleware()
+        {
+            initializer = new UIInitializer(TempReflection.ObjectFields(typeof(T)));
+        }
+        public override DataGridItem Create()
+        {
+            var t = new T();
+            initializer.Reset(t);
+            return t;
+        }
+        public Action<T, U> Invoke;
+        public override void Update(DataGridItem obj, DataGridItemContext dat)
+        {
+            if (Invoke != null)
+                Invoke(obj as T, dat as U);
+        }
     }
     public class DataGrid:Composite
     {
@@ -30,7 +61,18 @@ namespace huqiang.UIComposite
         FakeStruct HeadMod;
         FakeStruct Drag;
         FakeStruct Line;
-        public List<DataGridColumn> BindingData = new List<DataGridColumn>();
+        Vector2 contentSize;
+        Vector2 headSize;
+        float itemY;
+        List<DataGridColumn> columns = new List<DataGridColumn>();
+        public List<DataGridColumn> BindingData { get => columns; }
+        public SwapBuffer<DataGridItem, DataGridItemContext> swap;
+        QueueBuffer<DataGridItem> queue;
+        public DataGrid()
+        {
+            swap = new SwapBuffer<DataGridItem, DataGridItemContext>(1024);
+            queue = new QueueBuffer<DataGridItem>(256);
+        }
         public override void Initial(FakeStruct mod, UIElement element)
         {
             base.Initial(mod, element);
@@ -38,11 +80,101 @@ namespace huqiang.UIComposite
             ItemMod = HGUIManager.FindChild(mod, "Item");
             Drag = HGUIManager.FindChild(mod, "Drag");
             Line = HGUIManager.FindChild(mod, "Line");
+            unsafe
+            {
+                headSize = ((TransfromData*)HeadMod.ip)->size;
+                itemY = ((TransfromData*)ItemMod.ip)->size.y;
+            }
         }
 
-        void Order()
+        public void Refresh()
+        {
+            float x = 0;
+            for (int i = 0; i < columns.Count; i++)
+                x += columns[i].width;
+            contentSize.x = x;
+            OrderHead();
+            if (columns.Count > 0)
+                contentSize.y = itemY * columns[0].datas.Count;
+        }
+        void CreateHead()
         {
 
+        }
+        void UpdateItem(Vector3 pos,DataGridItemContext data)
+        {
+            var item = swap.Exchange((o, e) => { return o.Context == e; }, data);
+            if(item==null)
+            {
+
+            }
+        }
+        DataGridItem CreateItem()
+        {
+            DataGridItem it = queue.Dequeue();
+            if (it != null)
+            {
+                it.target.SetActive(true);
+                return it;
+            }
+            if (creator != null)
+            {
+                var t = creator.Create();
+                t.target = HGUIManager.GameBuffer.Clone(ItemMod, creator.initializer);
+                var trans = t.target.transform;
+                trans.SetParent(Enity.transform);
+                trans.localScale = Vector3.one;
+                trans.localRotation = Quaternion.identity;
+                return t;
+            }
+            else
+            {
+                var go = HGUIManager.GameBuffer.Clone(ItemMod);
+                var trans = go.transform;
+                trans.SetParent(Enity.transform);
+                trans.localScale = Vector3.one;
+                trans.localRotation = Quaternion.identity;
+                DataGridItem a = new DataGridItem();
+
+                return a;
+            }
+        }
+        void CreateLine()
+        {
+
+        }
+        void OrderHead()
+        {
+
+        }
+        void OrderItem()
+        {
+
+        }
+        void OrderLine()
+        {
+
+        }
+
+        public void AddColumn(DataGridColumn column)
+        {
+
+        }
+        public void AddRow()
+        {
+
+        }
+        public void RemoveRow()
+        {
+
+        }
+        DGConstructor creator;
+        public void SetItemUpdate<T, U>(Action<T, U> action)
+            where T : DataGridItem, new() where U : DataGridItemContext, new()
+        {
+            var m = new DGMiddleware<T, U>();
+            m.Invoke = action;
+            creator = m;
         }
     }
 }

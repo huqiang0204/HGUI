997b5c484260c9f047b18c90aa93a5849140be13
huqiang <huqiang1990@outlook.com>
2020-03-16 11:48:08 +0800

    fix

---
+++ Core/Data/JsonExtand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace huqiang.Data
+{
+    public class JsonExtand
+    {
+        public static string ToJson(object[] array)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append('[');
+            int l = array.Length;
+            for (int i = 0; i < l; i++)
+            {
+                if (i > 0)
+                    sb.Append(',');
+                sb.Append(JsonUtility.ToJson(array[i]));
+            }
+            sb.Append(']');
+            return sb.ToString();
+        }
+        public static T[] FromJson<T>(string jsonArray) where T : class, new()
+        {
+            string str = "{\"d\":" + jsonArray + "}";
+            Vessel<T> vessel = JsonUtility.FromJson<Vessel<T>>(str);
+            return vessel.d;
+        }
+    }
+    [Serializable]
+    class Vessel<T> where T : class, new()
+    {
+        public T[] d;
+    }
+}

ef690d2ccb2251537d38f48703de9c50a304db3d
weark <weark@go.com>
2020-07-17 15:00:40 +0800

    fix

---
+++ Core/Data/Msg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace huqiang.Data
+{
+    [Serializable]
+    public class Msg
+    {
+        public Int32 Type;
+        public Int32 Cmd;
+        public Int32 Error;
+        public string Args;
+    }
+}

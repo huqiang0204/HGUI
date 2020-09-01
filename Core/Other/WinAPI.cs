ba9d42d65861e3a97af2cae42f3166b94576c38f
weark <weark@go.com>
2020-09-01 18:09:45 +0800

    fix

--- Core/Other/WinAPI.cs
+++ Core/Other/WinAPI.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace huqiang
 {
@@ -14,6 +15,43 @@ namespace huqiang
         public static extern bool GetOpenFileName([In, Out] FileDlg ofd);
         [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
         public static extern bool GetSaveFileName([In, Out] FileDlg ofd);
+  
+        [DllImport("shell32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+        private static extern IntPtr SHBrowseForFolder([In, Out] OpenDialogDir ofn);
+
+        [DllImport("shell32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+        private static extern bool SHGetPathFromIDList([In] IntPtr pidl, [In, Out] char[] fileName);
+        public static string GetPathFromWindowsExplorer(string dialogtitle = "Select file path strength.")
+        {
+            try
+            {
+                OpenDialogDir ofn2 = new OpenDialogDir();
+                ofn2.pszDisplayName = new string(new char[2048]);
+                // 存放目录路径缓冲区  
+                ofn2.lpszTitle = dialogtitle; // 标题  
+                ofn2.ulFlags = 0x00000040; // 新的样式,带编辑框  
+                IntPtr pidlPtr = SHBrowseForFolder(ofn2);
+
+                char[] charArray = new char[2048];
+
+                for (int i = 0; i < 2048; i++)
+                {
+                    charArray[i] = '\0';
+                }
+
+                SHGetPathFromIDList(pidlPtr, charArray);
+                string res = new string(charArray);
+                res = res.Substring(0, res.IndexOf('\0'));
+                return res;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+            }
+
+            return string.Empty;
+        }
+
 #else
        public static bool GetOpenFileName([In, Out] FileDlg ofd) { return false; }
         public static bool GetSaveFileName([In, Out] FileDlg ofd) { return false; }
